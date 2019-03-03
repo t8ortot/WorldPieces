5fb0b3e4c6d43f3c369ce936ef6de0620e573436
James Clark <jimmyjclark@gmail.com>
2019-03-02 19:41:29 -0800

    home page

--- website/app.py
+++ website/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return render_template("Home.html")
 if __name__ == "__main__":
     app.run()
